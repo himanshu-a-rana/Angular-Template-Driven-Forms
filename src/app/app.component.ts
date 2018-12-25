b397a83062274c1fe64986a840282ddc73122c93
Himanshu Rana <himanshu.a.rana@gmail.com>
2018-12-25 11:35:24 +0530

    Form Validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,15 @@ import { Employee } from './models/employee.model';
 export class AppComponent {
   title = 'angular-template-driven-forms';
   languages = ['English', 'Spanish', 'Other'];
-  model = new Employee('Daria', 'Smith', true, 'w2', 'English');
+  // model = new Employee('Daria', 'Smith', true, 'w2', 'English');
+  model = new Employee('', '', true, 'w2', 'default');
+  hasPrimaryLanguageError = false;
+
+  validatePrimaryLanguage(event) {
+    if (this.model.primaryLanguage === 'default') {
+      this.hasPrimaryLanguageError = true;
+    } else {
+      this.hasPrimaryLanguageError = false;
+    }
+  }
 }
